cf8d0026a1a3fae5afdcbc1e20f53c418c7bf707
Sevak <sevak.mnatsakanyan@kviku.ru>
2020-12-10 05:41:26 +0400

    added console controller to register user

---
+++ console/controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace console\controllers;
+
+use common\models\User;
+use yii\web\Controller;
+
+/**
+ * Class UserController
+ *
+ * User controller to manage users via console command
+ *
+ * @package console\controllers
+ */
+class UserController extends Controller
+{
+    /**
+     * Method to register backend users
+     * @param $username
+     * @param $email
+     * @param $password
+     */
+    public function actionRegister($username, $email, $password)
+    {
+        $user = new User();
+        $user->username = $username;
+        $user->email = $email;
+        $user->status = User::STATUS_ACTIVE;
+        $user->setPassword($password);
+        $user->generateAuthKey();
+        $user->generateEmailVerificationToken();
+
+        if ($user->save()) {
+            echo 'User registered successfully';
+        } else {
+            echo 'error';
+        }
+    }
+}
